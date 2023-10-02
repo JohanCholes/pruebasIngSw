db8fc59e058f1ce2cc9d5e41007507ad75690161
Johan Choles <johan.choles@upb.edu.co>
2023-10-02 12:53:16 -0500

    feat: clean start up

--- lib/main.dart
+++ lib/main.dart
@@ -11,76 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 29, 66, 66),
-        body: Center(
-          child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(top: 100),
-                child: Text(
-                  "MINITIENDA",
-                  style: TextStyle(
-                    fontFamily: 'KronaOne',
-                    color: Colors.white,
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 280),
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 500),
-                height: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      spreadRadius: -16,
-                      blurRadius: 4,
-                      offset: const Offset(8, -20),
-                    ),
-                  ],
-                ),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Ingresa tu ID",
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(25.0)),
-                    filled: true,
-                    fillColor: const Color(0xffF2E3D5),
-                    contentPadding: const EdgeInsets.symmetric(
-                      vertical: 15.0,
-                      horizontal: 20.0,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 10),
-                child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24),
-                      ),
-                      backgroundColor: const Color.fromARGB(
-                          255, 15, 21, 23), // Color de fondo del botón
-                      padding: const EdgeInsets.all(14),
-                      textStyle: const TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
-                      fixedSize: const Size(280, 40),
-                    ),
-                    child: const Text("Ingresar")),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: Scaffold(),
     );
   }
 }
