a849077caf24544b0dde8ec8e4ecfe8927577e84
Johan Choles <johan.choles@upb.edu.co>
2023-10-02 13:39:06 -0500

    feat: login page bgcolor

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,7 +6,27 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Algo"),
+      backgroundColor: Color(0xFF3CA6A6),
+      body: Column(
+        children: [
+          Spacer(),
+          Text("MINITIENDA",
+          textAlign:  TextAlign.start,
+          style: TextStyle(
+            fontSize: 30,
+          ),
+          ),
+          Spacer(),
+          TextFormField(),
+          SizedBox(
+            height: 60,
+            width: 120,
+            child:  InkWell(),
+            
+          ),
+          Spacer(),
+        ],
+      ),
     );
   }
 }
